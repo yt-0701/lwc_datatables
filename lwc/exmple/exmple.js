d165b1ef3cc69db93b7a9d6314f31fde686e5136
yuki0501 <yutachan08+oldgithub@gmail.com>
2021-04-27 19:28:20 +0900

    テスト用

--- lwc/exmple/exmple.js
+++ lwc/exmple/exmple.js
@@ -6,16 +6,13 @@ export default class Exmple extends LightningElement {
     connectedCallback() {
         // 親からデータを渡す
         this.headers = [
-            {name: 'row0', displayname: 'テキスト1', type: 'TEXT'},
-            {name: 'row1', displayname: 'テキスト2', type: 'Text'},
-            {name: 'row2', displayname: 'テキスト3', type: 'TEXT'},
-            {name: 'row3', displayname: '数字', type: 'Number'},
-            {name: 'row4', displayname: '日付', type: 'date'},
-            {name: 'row5', displayname: 'テキスト4', type: 'TEXT'}
+            {name: 'row0', displayname: 'text1', type: 'TEXT'},
+            {name: 'row1', displayname: 'text2', type: 'Text'},
+            {name: 'row2', displayname: '日付', type: 'date'},
         ];
         this.items = [
-            {row0: 'aa', row1: 'bb', row2: 'cc', row3: 14, row4: '2020/12/02', row5: 'dd'},
-            {row0: '21', row1: '22', row2: '23', row3: 24, row4: '2020/09/08', row5: '26'},
+            {row0: 'aa', row1: 'cc', row2: '2020/12/02'},
+            {row0: '21', row1: '23', row2: '2020/09/08'},
         ];
     }
 }
